9134cf57aef937cc324325b2fd77c52a79bfe00b
iwatanabee <issei.watanabe0708@gmail.com>
2022-12-16 11:17:48 +0900

    first commit

---
+++ Assets/SampleShooting/RocketController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.01f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.01f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
